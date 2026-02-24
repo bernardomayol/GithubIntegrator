c74f631695285e7a31f1b76eac254d36a7e4a5dc
Bernardo Mayol <bernardomayol@yahoo.com>
2026-02-23 17:26:38 -0700

    Cambios 1

--- Gh.ControllersApi/Controllers/GitHubController.cs
+++ Gh.ControllersApi/Controllers/GitHubController.cs
@@ -6,13 +6,13 @@ namespace Gh.ControllersApi.Controllers
     [Authorize]
     [ApiController]
     [Route("api/v1/github")]
-    public class GitHubController : Microsoft.AspNetCore.Mvc.ControllerBase
+    public class GitHubController : ControllerBase
     {
         private readonly IHttpClientFactory _factory;
         public GitHubController(IHttpClientFactory factory) => _factory = factory;
 
-        [Microsoft.AspNetCore.Mvc.HttpGet("search")]
-        public async Task<IActionResult> Search([Microsoft.AspNetCore.Mvc.FromQuery] string q, [Microsoft.AspNetCore.Mvc.FromQuery] int page, [Microsoft.AspNetCore.Mvc.FromQuery] int pageSize)
+        [HttpGet("search")]
+        public async Task<IActionResult> Search([FromQuery] string q, [FromQuery] int page, [FromQuery] int pageSize)
         {
             var client = _factory.CreateClient("github");
             var resp = await client.GetAsync($"search/repositories?q={Uri.EscapeDataString(q)}&page={page}&per_page={pageSize}");
